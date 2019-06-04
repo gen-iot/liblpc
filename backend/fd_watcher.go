ea2e616609bc56d90e26ee2a1845551179378560
Suzhen <suzhen@homeinpocket.net>
2019-06-04 10:40:21 +0800

    fd_watcher

--- backend/fd_watcher.go
+++ backend/fd_watcher.go
@@ -1,6 +1,9 @@
 package backend
 
-import "syscall"
+import (
+	"container/list"
+	"syscall"
+)
 
 type baseEventWatcher struct {
 	fd    int
@@ -21,9 +24,11 @@ func (this *baseEventWatcher) SetEvent(event uint32) {
 
 type FdWatcher struct {
 	baseEventWatcher
+	readBuffer []byte
+	writeQ     *list.List
 }
 
-func NewFdWatcher(fd int, closeExec bool) *FdWatcher {
+func NewFdWatcher(fd int, closeExec bool, readBuffer []byte) *FdWatcher {
 	if closeExec {
 		syscall.CloseOnExec(fd)
 	}
@@ -31,6 +36,11 @@ func NewFdWatcher(fd int, closeExec bool) *FdWatcher {
 	w := new(FdWatcher)
 	w.fd = fd
 	w.event = syscall.EPOLLIN
+	if len(readBuffer) == 0 {
+		readBuffer = make([]byte, 1024*4)
+	}
+	w.readBuffer = readBuffer
+	w.writeQ = list.New()
 	return w
 }
 
@@ -46,26 +56,59 @@ func (this *FdWatcher) WantWrite() {
 	this.event |= syscall.EPOLLOUT
 }
 
-func (this *FdWatcher) OnRead() error {
+func (this *FdWatcher) OnRead(data []byte, len int) error {
 	return nil
 }
 
-func (this *FdWatcher) OnWrite() error {
+func (this *FdWatcher) Write(data []byte) error {
+	this.writeQ.PushBack(data)
 	return nil
+}
+
+func WOULDBLOCK(err error) bool {
+	if err == nil {
+		return false
+	}
+	return err == syscall.EWOULDBLOCK || err == syscall.EAGAIN
 }
 
 func (this *FdWatcher) OnEvent(event uint32) {
-	if event&syscall.EPOLLIN != 0 {
-		//read
-		for {
-			err := this.OnRead()
-		}
-	}
 
 	if event&syscall.EPOLLOUT != 0 {
 		//writable
-		for {
-			err := this.OnWrite()
+		for ; this.writeQ.Len() != 0; {
+			front := this.writeQ.Front()
+			dataWillWrite := front.Value.([]byte)
+
+			nWrite, err := syscall.SendmsgN(this.fd, dataWillWrite, nil, nil, syscall.MSG_NOSIGNAL)
+			if err != nil {
+				if WOULDBLOCK(err) {
+					dataWillWrite = dataWillWrite[nWrite:]
+					break
+				}
+				//todo onclose
+				return
+			}
+
+			this.writeQ.Remove(front)
 		}
 	}
+
+	if event&syscall.EPOLLIN != 0 {
+		//read
+		for {
+			nRead, _, err := syscall.Recvfrom(this.fd, this.readBuffer, syscall.MSG_NOSIGNAL)
+			if err != nil && WOULDBLOCK(err) {
+				//todo onclose
+				return
+			}
+
+			err = this.OnRead(this.readBuffer, nRead)
+			if err != nil {
+				return
+			}
+
+		}
+	}
+
 }
