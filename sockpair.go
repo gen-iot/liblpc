dec1d6153e8c64b29951a03ff2c48ee149b6efe5
Suzhen <suzhen@homeinpocket.net>
2019-08-24 15:53:45 +0800

    :wrench:  add forklock in make sockpair

--- sockpair.go
+++ sockpair.go
@@ -9,7 +9,9 @@ import (
 // fd[1] for child process
 // nonblock : set socket nonblock
 func MakeIpcSockpair(nonblock bool) (fds [2]int, err error) {
-	fds, err = syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
+	syscall.ForkLock.Lock()
+	defer syscall.ForkLock.Unlock()
+	fds, err = unix.Socketpair(unix.AF_UNIX, unix.SOCK_STREAM, 0)
 	if err != nil {
 		return
 	}
